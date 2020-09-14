5b382c7dc0bf3febfa629f4a3156edadbaa1e8b0
mengsuenyan <mengsuenyan@163.com>
2020-09-14 16:09:42 +0800

    [ADD] add RandError/RandErrKind for rand operations; [MOD] modify the return type from the Option to Result for the Seed/Source trait interface;

--- src/rand/default_rand_amd64.rs
+++ src/rand/default_rand_amd64.rs
@@ -1,4 +1,4 @@
-use crate::rand::{Source, Seed};
+use crate::rand::{Source, Seed, RandError, RandErrKind, Result};
 
 #[cfg(target_arch = "x86_64")]
 use std::arch::x86_64 as march;
@@ -9,52 +9,57 @@ use std::arch::x86 as march;
 pub struct DefaultRand {}
 
 impl DefaultRand {
-    pub fn new() -> Self {
+    pub fn new<Sd: Seed>(_sd: &impl Seed) -> Self {
         DefaultRand {}
     }
     
     #[target_feature(enable = "rdrand")]
-    unsafe fn gen_u32(&mut self) -> Option<u32> {
+    unsafe fn gen_u32(&mut self) -> Result<u32> {
         let mut out = 0;
         if march::_rdrand32_step(&mut out) > 0 {
-            Some(out)
+            Ok(out)
         } else {
-            None
+            Err(RandError::new(RandErrKind::NoNewRandNumberGen, ""))
         }
     }
 
     #[cfg(target_arch = "x86")]
     #[target_feature(enable = "rdrand")]
-    unsafe fn gen_u64(&mut self) -> Option<u64> {
-        self.gen_u32().zip(self.gen_u32()).map(|(low, high)| {
-            (low as u64) | ((high as u64) << 32)
-        })
+    unsafe fn gen_u64(&mut self) -> Result<u64> {
+        match self.gen_u32() {
+            Ok(low) => self.gen_u32().map(|high| {
+                Ok((low as u64) | ((high as u64) << 32))
+            }),
+            Err(e) => Err(e),
+        }
     }
 
     #[cfg(target_arch = "x86_64")]
     #[target_feature(enable = "rdrand")]
-    unsafe fn gen_u64(&mut self) -> Option<u64> {
+    unsafe fn gen_u64(&mut self) -> Result<u64> {
         let mut out = 0;
         if march::_rdrand64_step(&mut out) > 0 {
-            Some(out)
+            Ok(out)
         } else {
-            None
+            Err(RandError::new(RandErrKind::NoNewRandNumberGen, ""))
         }
     }
 }
 
 impl Source for DefaultRand {
-    fn set_seed<Sd: Seed>(&mut self, _sd: &Sd) {
+    fn set_seed<Sd: Seed>(&mut self, _sd: &Sd) -> Result<()> {
+        Ok(())
     }
 
-    fn reset(&mut self) {
+    fn reset(&mut self) -> Result<()> {
+        Ok(())
     }
 
-    fn gen_u32(&mut self) -> Option<u32> {
+    fn gen_u32(&mut self) -> Result<u32> {
         unsafe {self.gen_u32()}
     }
 
-    fn gen_u64(&mut self) -> Option<u64> {
+    fn gen_u64(&mut self) -> Result<u64> {
         unsafe {self.gen_u64()}
     }
 }
