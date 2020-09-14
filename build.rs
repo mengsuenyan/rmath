9380b3f2a7bcbec35330db1147d9b9ba7efd2931
mengsuenyan <mengsuenyan@163.com>
2020-09-14 12:52:16 +0800

    [ADD] add Seed/Source trait for the random seed and random number generation; [ADD] add DefaultRand/DefaultSeed for the x86/x86_64 platform;

--- build.rs
+++ build.rs
@@ -1,10 +1,19 @@
 
 fn main() {
+    if (std::mem::size_of::<usize>() != std::mem::size_of::<u32>()) && 
+        (std::mem::size_of::<usize>()) != std::mem::size_of::<u64>() {
+        panic!("The rmath does support the 32bit/64bit system");
+    }
+    
     if std::is_x86_feature_detected!("avx2") {
         println!("cargo:rustc-cfg=rmath_avx2=\"support\"");
     }
-
-    // if std::is_x86_feature_detected!("rdrand") {
-    //     println!("cargo:rustc-cfg=rdrand_is_support");
-    // }
+    
+    if std::is_x86_feature_detected!("rdseed") {
+        println!("cargo:rustc-cfg=rmath_rdseed=\"support\"");
+    }
+    
+    if std::is_x86_feature_detected!("rdrand") {
+        println!("cargo:rustc-cfg=rmath_rdrand=\"support\"");
+    }
 }
