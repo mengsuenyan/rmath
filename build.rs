cdced00a5e67b0cc2ca1ed210543691d1be69dc0
mengsuenyan <mengsuenyan@163.com>
2020-09-16 14:12:04 +0800

    [ADD] modify build.rs that limit the rmath to compile successfully only on the Windows or Linux system;

--- build.rs
+++ build.rs
@@ -1,5 +1,17 @@
 
 fn main() {
+    let os = {
+        #[cfg(target_os = "windows")]
+            {1}
+        #[cfg(target_os = "linux")]
+            {2}
+        #[cfg(not(any(target_os = "windows", target_os = "linux")))]
+            {0}
+    };
+    if os < 1 {
+        panic!("The rmath only support the Windows/Linux system");
+    }
+    
     if (std::mem::size_of::<usize>() != std::mem::size_of::<u32>()) && 
         (std::mem::size_of::<usize>()) != std::mem::size_of::<u64>() {
         panic!("The rmath does support the 32bit/64bit system");
