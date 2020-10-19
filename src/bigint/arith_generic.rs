9df08fe8f700ed774711f579a6fdee8e5502a036
mengsuenyan <mengsuenyan@163.com>
2020-10-19 13:18:26 +0800

    [ADD] add basic arithmetic for vector

--- src/bigint/arith_generic.rs
+++ src/bigint/arith_generic.rs
@@ -33,89 +33,74 @@ pub(super) fn div_ww(u1: u32, u0: u32, v: u32) -> (u32, u32) {
 
 /// z = x + y   
 /// return carry   
-pub(super) fn add_vv(z: &mut Vec<u32>, x: &Vec<u32>, y: &Vec<u32>) -> u32 {
-    z.clear();
-    x.iter().zip(y.iter()).fold(0, |c, (&xt, &yt)| {
+pub(super) fn add_vv(z: &mut [u32], x: &[u32], y: &[u32]) -> u32 {
+    z.iter_mut().zip(x.iter().zip(y.iter())).fold(0, |c, (zt, (&xt, &yt))| {
         let (tt0, cc0) = xt.overflowing_add(yt);
         let (tt, cc) = tt0.overflowing_add(c);
-        z.push(tt);
+        *zt = tt;
         if cc0 || cc {1} else {0}
     })
 }
 
 /// z = x - y   
 /// return carry   
-pub(super) fn sub_vv(z: &mut Vec<u32>, x: &Vec<u32>, y: &Vec<u32>) -> u32 {
-    z.clear();
-    x.iter().zip(y.iter()).fold(0, |c, (&xt, &yt)| {
+pub(super) fn sub_vv(z: &mut [u32], x: &[u32], y: &[u32]) -> u32 {
+    z.iter_mut().zip(x.iter().zip(y.iter())).fold(0, |c, (zt, (&xt, &yt))| {
         let (tt0, cc0) = xt.overflowing_sub(yt);
         let (tt, cc) = tt0.overflowing_sub(c);
-        z.push(tt);
+        *zt = tt;
         if cc0 || cc {1} else {0}
     })
 }
 
-pub(super) fn add_vw(z: &mut Vec<u32>, x: &Vec<u32>, y: u32) -> u32 {
-    z.clear();
-    x.iter().fold(y, |c, &xt| {
+pub(super) fn add_vw(z: &mut [u32], x: &[u32], y: u32) -> u32 {
+    z.iter_mut().zip(x.iter()).fold(y, |c, (zt, &xt)| {
         let (tt, cc) = xt.overflowing_add(c);
-        z.push(tt);
+        *zt = tt;
         if cc {1} else {0}
     })
 }
 
-pub(super) fn add_vw_large(z: &mut Vec<u32>, x: &Vec<u32>, y: u32) -> u32 {
-    let mut c = y;
-    
-    z.clear();
-    for (i, &xt) in x.iter().enumerate() {
+pub(super) fn add_vw_large(z: &mut [u32], x: &[u32], y: u32) -> u32 {
+    z.iter_mut().zip(x.iter()).fold(y, |c, (zt, &xt)| {
         if c == 0 {
-            z.extend_from_slice(&x.as_slice()[i..]);
-            return 0;
+            *zt = xt;
+            0
         } else {
             let (tt, cc) = xt.overflowing_add(c);
-            c = if cc{1} else {0};
-            z.push(tt);
+            *zt = tt;
+            if cc {1} else {0}
         }
-    }
-    c
+    })
 }
 
-pub(super) fn sub_vw(z: &mut Vec<u32>, x: &Vec<u32>, y: u32) -> u32 {
-    z.clear();
-    x.iter().fold(y, |c, &xt| {
+pub(super) fn sub_vw(z: &mut [u32], x: &[u32], y: u32) -> u32 {
+    z.iter_mut().zip(x.iter()).fold(y, |c, (zt, &xt)| {
         let (tt, cc) = xt.overflowing_sub(c);
-        z.push(tt);
+        *zt = tt;
         if cc {1} else {0}
     })
 }
 
-pub(super) fn sub_vw_large(z: &mut Vec<u32>, x: &Vec<u32>, y: u32) -> u32 {
-    let mut c = y;
-    z.clear();
-    
-    for (i, &xt) in x.iter().enumerate() {
+pub(super) fn sub_vw_large(z: &mut [u32], x: &[u32], y: u32) -> u32 {
+    z.iter_mut().zip(x.iter()).fold(y, |c, (zt, &xt)| {
         if c == 0 {
-            z.extend_from_slice(&x.as_slice()[i..]);
-            return 0;
+            *zt = xt;
+            0
         } else {
             let (tt, cc) = xt.overflowing_sub(c);
-            c = if cc{1} else {0};
-            z.push(tt);
+            *zt = tt;
+            if cc {1} else {0}
         }
-    }
-    
-    c
+    })
 }
 
-pub(super) fn shl_vu(z: &mut Vec<u32>, x: &Vec<u32>, s: usize) -> u32 {
-    z.clear();
+pub(super) fn shl_vu(z: &mut [u32], x: &[u32], s: usize) -> u32 {
     if s == 0 || x.is_empty(){
-        z.extend_from_slice(x.as_slice());
+        z.copy_from_slice(x);
         return 0;
     }
     
-    z.resize(x.len(), 0);
     let s = s & 31;
     let ss = (32 - s) & 31;
     x.iter().rev().skip(1).zip(x.iter().rev().zip(z.iter_mut().rev())).for_each(|(&xt0, (&xt1, zt))| {
@@ -125,13 +110,11 @@ pub(super) fn shl_vu(z: &mut Vec<u32>, x: &Vec<u32>, s: usize) -> u32 {
     x.last().unwrap() >> s
 }
 
-pub(super) fn shr_vu(z: &mut Vec<u32>, x: &Vec<u32>, s: usize) -> u32 {
-    z.clear();
+pub(super) fn shr_vu(z: &mut [u32], x: &[u32], s: usize) -> u32 {
     if s == 0 || x.is_empty(){
-        z.extend_from_slice(x.as_slice());
+        z.copy_from_slice(x);
         return 0;
     }
-    z.resize(x.len(), 0);
     let s = s & 31;
     let ss = (32 - s) & 31;
     
@@ -142,17 +125,16 @@ pub(super) fn shr_vu(z: &mut Vec<u32>, x: &Vec<u32>, s: usize) -> u32 {
     x[0] << ss
 }
 
-pub(super) fn mul_add_vww(z: &mut Vec<u32>, x: &Vec<u32>, y: u32, r: u32) -> u32 {
-    z.clear();
-    x.iter().fold(r, |c, &xt| {
-        let (cc, zt) = mul_add_www(xt, y, c);
-        z.push(zt);
+pub(super) fn mul_add_vww(z: &mut [u32], x: &[u32], y: u32, r: u32) -> u32 {
+    z.iter_mut().zip(x.iter()).fold(r, |c, (zt, &xt)| {
+        let (cc, tt) = mul_add_www(xt, y, c);
+        *zt = tt;
         cc
     })
 }
 
 /// z = x*y + z
-pub(super) fn add_mul_vvw(z: &mut Vec<u32>, x: &Vec<u32>, y: u32) -> u32 {
+pub(super) fn add_mul_vvw(z: &mut [u32], x: &[u32], y: u32) -> u32 {
     z.iter_mut().zip(x.iter()).fold(0, |c, (zt, &xt)| {
         let (z1, z0) = mul_add_www(xt, y, *zt);
         let (lo, cc) = z0.overflowing_add(c);
@@ -161,9 +143,7 @@ pub(super) fn add_mul_vvw(z: &mut Vec<u32>, x: &Vec<u32>, y: u32) -> u32 {
     })
 }
 
-pub(super) fn div_wvw(z: &mut Vec<u32>, xn: u32, x: &Vec<u32>, y: u32) -> u32 {
-    z.clear();
-    z.resize(x.len(), 0);
+pub(super) fn div_wvw(z: &mut [u32], xn: u32, x: &[u32], y: u32) -> u32 {
     z.iter_mut().zip(x.iter()).fold(xn, |r, (zt, &xt)| {
         let (tmpz, tmpr) = div_ww(r, xt, y);
         *zt = tmpz;
